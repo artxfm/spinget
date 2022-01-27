74b666b0641a8c7faed5e562647d1594058feaf2
Mathias Kolehmainen <mkolehmainen@appliedinvention.com>
2022-01-27 15:51:04 -0500

    work in progress to handle new format

--- spinget.py
+++ spinget.py
@@ -2,14 +2,14 @@
 
 # usage:
 #
-#  spinget.py 11/20/2021 10:00 2
-#     capture 2 hours starting at 10:00pm on November 20, 2021.
+#  spinget.py 11/20/2021 10:00
+#     capture show starting at 10:00pm on November 20, 2021.
 #
-# Requires python3 and the requests lib.
-# `pip install requests` should do it.
+# Requires python3 and:
+#   - requests `pip install requests`
+#   - m3u8 `pip instrall m3u8`
 #
-#
-# version 1, by mathias for WXOX
+# version 2, by mathias for WXOX
 
 
 import argparse
@@ -17,100 +17,91 @@ import sys
 import os
 from datetime import datetime, date, time, timezone, timedelta
 import requests
+import m3u8
 
 
-BASEURL="https://ark-wxox.s3.us-east-1.amazonaws.com/"
-URLTMPL = BASEURL+"WXOX-{0}.mp3"
+INDEXURL="https://ark2.spinitron.com/ark2/WXOX-{0}/index.m3u8" # pass in UTC timestamp
 
 
-# Get a 5 minute chunk using the passed datetime (which must be multiple of 5 minutes).
-# Returns the name of file written or empty string if request fails.
-def get5MinChunk(utcdt):
-    chunkID = utcdt.strftime("%Y%m%dT%H%M00Z")  
-    url = URLTMPL.format(chunkID)
-    print("fetch chunk {0}".format(chunkID))
-    print("  => ", url)
-
-    headers = {
-        'Connection': 'keep-alive',
-        'sec-ch-ua': '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
-        'DNT': '1',
-        'sec-ch-ua-mobile': '?0',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.#54 Safari/537.36',
-        'sec-ch-ua-platform': '"macOS"',
-        'Accept': '*/*',
-        'Origin': 'https://spinitron.com',
-        'Sec-Fetch-Site': 'cross-site',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://spinitron.com/',
-        'Accept-Language': 'en-US,en;q=0.9'
-    }
-
-    r = requests.get(url, headers=headers, stream=True)
-    if r.status_code != requests.codes.ok:
-        print("  * request failed: {0}".format(r.status_code))
-        return ""
-
-    chunkFile = "wxox_{0}.tmp.mp3".format(chunkID)
-    print("  output to: {0}".format(chunkFile))
-    with open(chunkFile, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=128):
-            fd.write(chunk)
-    return chunkFile
+def segtofile(n, seguri):
+    chunkID = seguri.split("/")[-1]
+    return "wxox_%0.5d_%s.tmp.mpeg" % (n, chunkID)
 
 
-
-# concat all the files listed in `input` to a file named `output`.
-# If `remove` is true then also delete the input files.
-def concat(inputs, output, rm):
+def concat(seglist, output, rm):
+    print("concatenating %d segments..." % len(seglist))
     with open(output, 'wb') as fdout:
-        for fn in inputs:
+        n = 0
+        for seguri in seglist:
+            n = n + 1
+            fn = segtofile(n, seguri)
             with open(fn, 'rb') as fdin:
                 fdout.write(fdin.read())
             if rm:
                 os.remove(fn)
 
-
-# getHour gets an hour of audio by downloading each 5min chunk. Then combines
-# all the chunks into one file, returning the filename.  Returns empty string on
-# error.
-def getHour(utcdt):
-    hourID = utcdt.strftime("%Y%m%dT%H%M00Z")  
-    chunks = []
-    cur = utcdt
-    for n in range(0, 12):
-        cfile = get5MinChunk(cur)
-        if len(cfile) > 0:
-            chunks.append(cfile)
-        cur = cur + timedelta(minutes=5)
-    if len(chunks) == 0:
-        return ""
-    outfile = "wxox_{0}.hr.mp3".format(hourID)
-    concat(chunks, outfile, True)
-    return outfile
+# Takes a list of segment URIs and dowloads them one at a time.
+def download(seglist):
+    n = 0
+    for seguri in seglist:
+        n = n + 1
+        print("fetch seg %d/%d  %s" % (n, len(seglist), seguri))
+        r = requests.get(seguri, stream=True)
+        if r.status_code != requests.codes.ok:
+            print("  * request failed: {0}".format(r.status_code))
+            return False
+        chunkFile = segtofile(n, seguri)
+        # print("  output to: {0}".format(chunkFile))
+        with open(chunkFile, 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)
+    return True
 
 
+# Given string format of time from user, convert to a real datetime object in
+# UTC zone.
+def makets(t):
+    localstamp  = datetime.strptime(t, "%m/%d/%Y %H:%M")
+    tt = localstamp.timetuple()
+    if not(tt.tm_min == 0 or tt.tm_min == 5):
+        print("ERROR: time must be multiple of 5 minutes")
+        sys.exit(1)
+    return localstamp.astimezone(timezone.utc)
 
-# getHours gets `n` hours of audio starting with timestamp `utcdt` (timestamp in UTC).
-# Returns mp3 filename, or empty string if error.
-def getHours(utcdt, n):
-    showID = utcdt.strftime("%Y%m%dT%H%M00Z")  
-    hours = []
-    cur = utcdt
-    for i in range(0, n):
-        hourfile = getHour(cur)
-        if len(hourfile) > 0:
-            hours.append(hourfile)
-        cur = cur + timedelta(hours=1)
-    if len(hours) == 0:
-        return ""
-    outfile = "wxox_{0}.mp3".format(showID)
-    if len(hours) == 1:
-        os.rename(hours[0], outfile)
-    else:
-        concat(hours, outfile, True)
-    return outfile
+
+# use the indexes to get all segments necessary for the number of hours
+# requestd. Returns a list of segment URIs
+def loadsegs(stamp, hours):
+    curts = stamp
+    segs = []
+    accum = 0 # seconds
+    required = hours * 60 * 60 # seconds
+
+    while accum < required:
+        showtime = curts.strftime("%Y%m%dT%H%M00Z")
+        print("fetching index for {0}".format(showtime))
+        playlist = m3u8.load(INDEXURL.format(showtime))
+        if len(playlist.segments) == 0:
+            print("no playlist data found")
+            return []
+        total_secs = 0 # seconds from this playlist
+        for seg in playlist.segments:
+            # print("    {0}  {1}s  {2}".format(showtime, seg.duration, seg.uri))
+            if (total_secs + seg.duration) > (30 * 60): # have we exeecded 30mins?
+                break
+            segs.append(seg.uri)
+            total_secs = total_secs + seg.duration
+        if total_secs == 0:
+            print("playlist has no content")
+            return []
+        accum = accum + total_secs
+        print(" --> has {0} seconds (need {1})".format(total_secs, required - accum))
+        if accum >= required:
+            break
+        curts = curts + timedelta(minutes=30) # grab index starting at next half hour
+    return segs
+
+
 
 
 
@@ -118,28 +109,29 @@ def getHours(utcdt, n):
 parser = argparse.ArgumentParser()
 parser.add_argument('date', metavar='MM/DD/YYYY', help='The show date')
 parser.add_argument('time', metavar="HH:MM", help='Starting time')
-parser.add_argument('count', type=int, metavar="N", help='Number of hours to retrieve (up to 4)')
+parser.add_argument('count', type=int, metavar="N", help="hours")
 args = parser.parse_args()
 
 hours = args.count
+if hours > 2 or hours < 1:
+    print("hours must be 1 or 2")
+
 timestamp = "{0} {1}".format(args.date, args.time)
+utcs = makets(timestamp)
 
-localstamp  = datetime.strptime(timestamp, "%m/%d/%Y %H:%M")
-tt = localstamp.timetuple()
-if not(tt.tm_min == 0 or tt.tm_min == 5):
-    print("ERROR: time must be multiple of 5 minutes")
-    sys.exit(1)
-if hours > 4 or hours < 1:
-    print("ERROR: hours must be betwen 1 and 4")
-    sys.exit(1)
+showID = utcs.strftime("%Y%m%dT%H%M00Z")
+print("show start is {0}".format(showID))
 
-utcstamp = localstamp.astimezone(timezone.utc)
-print("show start is {0}".format(utcstamp.strftime("%Y%m%dT%H%M00Z")))
-outfile = getHours(utcstamp, hours)
-if outfile == "":
-    print("Uh oh, nothing was retrieved!")
-    sys.exit(1)
-print("downloaded audio to {0}".format(outfile))
+outfile = "wxox_{0}_{1}h.mpeg".format(showID, hours)
+
+
+seglist = loadsegs(utcs, hours)
+if len(seglist) > 0:
+    print("downloading {0} segments...".format(len(seglist)))
+    if download(seglist):
+        concat(seglist, outfile, True) # set False to leave the downloaded files.
+
+
 
 
 
